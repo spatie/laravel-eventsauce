f9916dab3d489fb9a821c8c4be59085019408acc
freek <freek@spatie.be>
2019-01-21 23:00:53 +0100

    wip

--- config/eventsauce.php
+++ config/eventsauce.php
@@ -5,9 +5,13 @@ return [
      * We can generate  types, commands and events for you starting from a yaml file.
      * Here you can specify the input and the output.
      *
-     * More info on code generation here: https://eventsauce.io/docs/getting-started/create-events-and-commands
+     * More info on code generation here:
+     * https://eventsauce.io/docs/getting-started/create-events-and-commands
      */
     'code_generation' => [
-        ['input_yaml_file' => null, 'output_file' => null],
+        [
+            'input_yaml_file' => null,
+            'output_file' => null,
+        ]
     ],
 ];
